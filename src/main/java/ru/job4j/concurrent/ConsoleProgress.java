eb1042b8cca73d7ae03e93f677151df97d5eb701
Vitaliy123 <cpc1251@mail.ru>
2022-08-11 15:53:01 +0300

    job4j-40 3. Прерывание нити [#1019]
    Заменил ноль на константу.

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -4,6 +4,8 @@ package ru.job4j.concurrent;
  * Класс для вывода процесса загрузки в консоль
  */
 public class ConsoleProgress implements java.lang.Runnable {
+    private static final int ZERO = 0;
+    
     public static void main(String[] args) throws InterruptedException {
         Thread progress = new Thread(new ConsoleProgress());
         progress.start();
@@ -14,12 +16,12 @@ public class ConsoleProgress implements java.lang.Runnable {
     @Override
     public void run() {
         char[] process = {'\\', '|', '/'};
-        int count = 0;
+        int count = ZERO;
         System.out.print("Loading ... |.");
         try {
             while (!Thread.currentThread().isInterrupted()) {
                 System.out.print("\r load: " + process[count++]);
-                count = count == process.length ? 0 : count;
+                count = count == process.length ? ZERO : count;
                 Thread.sleep(500);
             }
         } catch (InterruptedException e) {
