a28f271fd3b64c04d25d6b8535801ae2fc46f1a2
Vitaliy123 <cpc1251@mail.ru>
2022-08-12 18:11:02 +0300

    job4j-45 2. Модель памяти Java [#267917]
    реализация double check locking

---
+++ src/main/java/ru/job4j/synchro/DCLSingleton.java
@@ -0,0 +1,25 @@
+package ru.job4j.synchro;
+
+public final class DCLSingleton {
+
+    private static volatile DCLSingleton inst;
+
+    public static DCLSingleton instOf() {
+        if (inst == null) {
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
+        }
+        return inst;
+    }
+
+    private DCLSingleton() {
+    }
+
+    public static void main(String[] args) {
+        DCLSingleton singleton = instOf();
+        System.out.println(singleton);
+    }
+}
