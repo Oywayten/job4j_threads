da1fdef5676d0eeb56b6b4fc779204405fb20814
Vitaliy123 <cpc1251@mail.ru>
2022-08-13 22:16:36 +0300

    job4j-42 4. Скачивание файла с ограничением. [#144271]
    Добавил валидацию параметров в метод main(): если они не заданы или заданы не полностью - генерируется исключение.
    
    Изменил логику подсчета скорости.

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -14,8 +14,9 @@ import java.net.URL;
  */
 public class Wget implements Runnable {
     private static final int OFF = 0;
-    private static final int BUF_SIZE = 1024;
     private static final int OUT_OF = -1;
+    private static final int BUF_SIZE = 1024;
+    private static final int THOUSAND = 1000;
     private final String url;
     private final String file;
     private final int speed;
@@ -27,6 +28,10 @@ public class Wget implements Runnable {
     }
 
     public static void main(String[] args) throws InterruptedException {
+        if (args.length != 3) {
+            throw new IllegalArgumentException(
+                    "Must have 3 parameters: Specify source link, destination file and target speed");
+        }
         String url = args[0];
         String file = args[1];
         int speed = Integer.parseInt(args[2]);
@@ -41,20 +46,24 @@ public class Wget implements Runnable {
              FileOutputStream fileOutputStream = new FileOutputStream(file)) {
             byte[] dataBuffer = new byte[BUF_SIZE];
             int bytesRead;
+            int downloadData = 0;
             long preReadTime = System.currentTimeMillis();
             while ((bytesRead = in.read(dataBuffer, OFF, BUF_SIZE)) != OUT_OF) {
-                long postReadTime = System.currentTimeMillis();
-                long deltaTime = (postReadTime - preReadTime);
-                fileOutputStream.write(dataBuffer, OFF, bytesRead);
-                if (deltaTime < speed) {
-                    System.out.println(deltaTime);
-                    Thread.sleep(speed - deltaTime);
+                downloadData += bytesRead;
+                if (downloadData == speed) {
+                    long postReadTime = System.currentTimeMillis();
+                    long deltaTime = postReadTime - preReadTime;
+                    if (deltaTime < THOUSAND) {
+                        long pause = THOUSAND - deltaTime;
+                        Thread.sleep(pause);
+                    }
+                    downloadData = 0;
+                    preReadTime = System.currentTimeMillis();
                 }
-                preReadTime = System.currentTimeMillis();
+                fileOutputStream.write(dataBuffer, OFF, bytesRead);
             }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
-        long time = System.currentTimeMillis();
     }
 }
