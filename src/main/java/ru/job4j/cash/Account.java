9e60c4d4da4cd3683b9158bfffb5cda790d49820
Vitaliy123 <cpc1251@mail.ru>
2022-08-14 22:13:54 +0300

    job4j-51 3. Денежные переводы AccountStorage [#1104]
    Поправил сборку - перевел record в класс

--- src/main/java/ru/job4j/cash/Account.java
+++ src/main/java/ru/job4j/cash/Account.java
@@ -1,5 +1,48 @@
 package ru.job4j.cash;
 
-public record Account(int id, int amount) {
+import java.util.Objects;
+
+public final class Account {
+    private final int id;
+    private final int amount;
+
+    public Account(int id, int amount) {
+        this.id = id;
+        this.amount = amount;
+    }
+
+    public int id() {
+        return id;
+    }
+
+    public int amount() {
+        return amount;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        var that = (Account) obj;
+        return this.id == that.id
+                && this.amount == that.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, amount);
+    }
+
+    @Override
+    public String toString() {
+        return "Account["
+                + "id=" + id + ", "
+                + "amount=" + amount + ']';
+    }
+
 
 }
