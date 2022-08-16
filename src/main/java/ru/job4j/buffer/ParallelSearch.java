ed652e3cb431a71451447a72dea8914d822c71a1
Vitaliy123 <cpc1251@mail.ru>
2022-08-16 21:54:07 +0300

    job4j-55 2. Обеспечить остановку потребителя. [#66825]
    Класс обеспечивает остановку потока consumer за счет объявления потока демоном.
    
    Удалил лишние элементы.

---
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -0,0 +1,32 @@
+package ru.job4j.buffer;
+
+import ru.job4j.SimpleBlockingQueue;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (true) {
+                        System.out.println(queue.poll());
+                    }
+                }
+        );
+        consumer.setDaemon(true);
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        queue.offer(index);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+        ).start();
+    }
+}
